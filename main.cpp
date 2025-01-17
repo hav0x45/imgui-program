f08f01168b6552fbb49e50ad8082ca9c3f0b0675
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-18 01:09:46 +0200

    Committed something

--- main.cpp
+++ main.cpp
@@ -1,30 +1,27 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
+#define STB_TRUETYPE_IMPLEMENTATION
 #include <iostream>
 #include <imgui.h>
 #include <imgui.cpp>
-#include <imgui_widgets.cpp>
-#include <imgui_tables.cpp>
+#include <imconfig.h>
+#include <imgui_demo.cpp>
+#include <imgui_draw.cpp>
 #include <imgui_internal.h>
+#include <imgui_tables.cpp>
+#include <imgui_widgets.cpp>
 #include <imgui_impl_dx9.h>
-#include <imgui_impl_opengl3.h>
-#include <imgui_impl_opengl3.cpp>
+#include <imgui_impl_dx9.cpp>
 #include <imgui_impl_win32.h>
 #include <imgui_impl_win32.cpp>
-#include <imgui_draw.cpp>
-#include <imgui_demo.cpp>
-#include <imconfig.h>
-// #include <imstb_rectpack.h>
-// #include <imstb_textedit.h>
-// #include <imstb_truetype.h>
+#include <imstb_rectpack.h>
+#include <imstb_textedit.h>
+#include <imstb_truetype.h>
 
 int main() {
 
     std::cout << "This is my ImGui Cheat!" << std::endl;
 
     const char* title = "Title";
-
-    ImGui::Begin(title);
-    ImGui::End();
 
     return 0;
 }
