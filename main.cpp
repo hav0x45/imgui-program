def618be0aa0007ba72a52a6e923866d6d820bb0
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-17 15:38:01 +0200

    Some coding

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
+#define IMGUI_DEFINE_MATH_OPERATORS
 #include <iostream>
-#include <imgui.cpp>
 #include <imgui.h>
-#include <glfw3.h>
-using namespace std;
+#include <imgui.cpp>
+#include <imgui_widgets.cpp>
+#include <imgui_tables.cpp>
+#include <imgui_internal.h>
+#include <imgui_impl_dx9.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_opengl3.cpp>
+#include <imgui_impl_win32.h>
+#include <imgui_impl_win32.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_demo.cpp>
+#include <imconfig.h>
+// #include <imstb_rectpack.h>
+// #include <imstb_textedit.h>
+// #include <imstb_truetype.h>
 
 int main() {
 
