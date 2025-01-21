e9b0ba32143d54e2be05f421d2e96179ef87e15a
Pekka Kukonlehto <pekkakukonlehto@outlook.com>
2025-01-21 09:37:35 +0200

    Fixed includes and errors

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
+#define IMGUI_DEFINE_MATH_OPERATORS
 #include <iostream>
 #include <imgui.h>
+#include <imgui.cpp>
+#include <imgui_draw.cpp>
+#include <imgui_tables.cpp>
+#include <imgui_widgets.cpp>
 
 int main() {
 
-    ImGui::Begin("Title");
+    std::cout << "Hello ImGui!" << std::endl;
+
+    bool* booleanthing;
+    const char* text = "Title";
+
+    ImGui::Begin(text, booleanthing, 0);
 
     return 0;
 }
